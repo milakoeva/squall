ccc54ab47304edb3bd92af25e31dff6b2aa8f852
Mila Koeva <mila@micrometrics.co>
2016-10-03 23:35:49 -0400

    Add location group class

---
+++ lib/squall/location_group.rb
@@ -0,0 +1,22 @@
+module Squall
+  # OnApp Hypervisor
+  class LocationGroup < Base
+    # Public: Lists all location groups.
+    #
+    # Returns an Array.
+    def list
+      req = request(:get, '/settings/location_groups.json')
+      req.collect { |loc| loc['location_group'] }
+    end
+
+    # Public: Retrieves location group info.
+    #
+    # id - The id of the location group
+    #
+    # Returns a Hash.
+    def show(id)
+      req = request(:get, "/settings/location_groups/#{id}.json")
+      req.first[1]
+    end
+  end
+end

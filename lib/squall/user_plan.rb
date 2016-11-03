4d0745ecd46418b8a2b52edb5a377fdccc6a73ae
Mila Koeva <mila@micrometrics.co>
2016-11-03 15:37:10 -0400

    Add resources to billing plan

--- lib/squall/user_plan.rb
+++ lib/squall/user_plan.rb
@@ -39,5 +39,23 @@ module Squall
       req = request(:post, '/billing/user/plans.json', default_params(options))
       req.first[1]
     end
+
+    # Public: Add resources to billing plan.
+    #
+    # options -
+    #           :resource_class - the name of the base resource you add to the billing plan in the following format: 
+    #                             Resource::[resource_name], where [resource_name] is HypervisorGroup for compute zone limits
+    #           :billing_plan_id - the ID of the billing plan
+    #           :target_type - the type of the group you add to the billing plan limits, for Limits for compute Zones it is Pack.
+    #           :target_id - the ID of the group (or zone) you add to billing plan limits
+    #
+    # Example
+    #
+    # 
+    # Returns a Hash.
+    def add_resource(id, options = {})
+      req = request(:post, "/billing/user/plans#{id}/resources.json", query: { resource: options } )
+      req.first[1]
+    end
   end
 end
